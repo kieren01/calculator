25a25ab3ad948a87d8873345ee9b8748e75f041f
kieren01 <kierentan1@gmail.com>
2024-02-13 19:30:27 +0800

    Added a static calculator display

---
+++ script.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+    return a + b;
+  };
+
+function subtract(a, b) {
+    return a - b;
+  };
+
+function multiply(a, b) {
+    return a * b; 
+  };
+
+function divide(a, b) {
+    return a / b;
+}
+
+let num1, num2, operator;
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            return(add(num1, num2));
+        case '-':
+            return(subtract(num1, num2));
+        case '*':
+            return(multiply(num1, num2));
+        case '/':
+            return(divide(num1, num2));
+        default:
+            return "Invalid operator";
+    }
+}
